8f78773df7bf641225741de2ef54cedaf4fc3c8d
Antony Courtney <antony.courtney@gmail.com>
2014-03-03 16:31:33 -0800

    basic modal create dialog

--- tabman.js
+++ tabman.js
@@ -141,22 +141,29 @@
     var headerId = managed ? 'managedWindows' : 'unmanagedWindows';
     var windowItem = makeElem( 'div', { classes: [ "windowInfo" ] } );
 
-    var windowHeader = makeElem( 'div', { classes: [ "windowHeader" ] } );
+    var windowHeader = makeElem( 'div', 
+      { classes: [ "nowrap", "singlerow", "oneRowContainer", "windowHeader" ] } );
 
     var windowTitleItem = makeElem( 'span', 
       { text: windowTitle, 
-        classes: [ "nowrap", "singlerow", "windowTitle" ],
+        classes: [ "windowList", "nowrap", "singlerow", "windowTitle" ],
         parent: windowHeader 
       });
 
     if( !managed ) {
       var windowManageButton = makeElem( 'button', 
-        { classes: [ "manage" ],
+        { classes: [ "manage", "header-button" ],
           attributes: { 'title': "Manage as Subject Window" },
           parent: windowHeader 
         });
       windowManageButton.onclick = function() {
         console.log( "Manage window '" + windowTitle + "'" );
+        var subjField = $( "#subject" );
+        subjField.val( windowTitle );
+        window.setTimeout( function() {
+          subjField[0].setSelectionRange( 0, windowTitle.length );
+        }, 0 );
+        $( "#manage-dialog" ).dialog( "open" );        
       }
     }
     var tabListItem = makeElem('div', { classes: [ "tablist" ] } );
@@ -164,7 +171,7 @@
       var tab = tabs[ i ];
 
       var tabItem = makeElem( 'div', 
-        { classes: [ "singlerow", "nowrap", "tabinfo" ] } );
+        { classes: [ "singlerow", "nowrap", "oneRowContainer", "tabinfo" ] } );
       if( tab.favIconUrl ) {
         var tabFavIcon = makeElem('img', { classes: [ "favicon" ], parent: tabItem } );
         tabFavIcon.setAttribute( 'src', tab.favIconUrl );
@@ -172,7 +179,7 @@
 
       var titleItem = makeElem( 'span', 
         { text: tab.title,
-          classes: [ "nowrap", "singlerow" ],
+          classes: [ "windowList", "nowrap", "singlerow" ],
           parent: tabItem
         });
 
@@ -214,13 +221,58 @@
     }          
   }
 
+  function initManageDialog() {
+    var subject = $("#subject"),
+        allFields = $( [] ).add( subject );
+    $("#manage-dialog").dialog({
+          autoOpen: false,
+          height: 200,
+          width: 280,
+          modal: true,
+          buttons: {
+            "Create": function() {
+              var bValid = true;
+              // allFields.removeClass( "ui-state-error" );
+
+/*
+              bValid = bValid && checkLength( name, "username", 3, 16 );
+              bValid = bValid && checkLength( email, "email", 6, 80 );
+              bValid = bValid && checkLength( password, "password", 5, 16 );
+
+              bValid = bValid && checkRegexp( name, /^[a-z]([0-9a-z_])+$/i, "Username may consist of a-z, 0-9, underscores, begin with a letter." );
+              // From jquery.validate.js (by joern), contributed by Scott Gonzalez: http://projects.scottsplayground.com/email_address_validation/
+              bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com" );
+              bValid = bValid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
+*/
+              if ( bValid ) {
+/*
+                $( "#users tbody" ).append( "<tr>" +
+                  "<td>" + name.val() + "</td>" +
+                  "<td>" + email.val() + "</td>" +
+                  "<td>" + password.val() + "</td>" +
+                "</tr>" );
+*/
+                $( this ).dialog( "close" );
+              }
+            },
+            Cancel: function() {
+              $( this ).dialog( "close" );
+            }
+          },
+          close: function() {
+            allFields.val( "" ).removeClass( "ui-state-error" );
+          }
+        }); 
+  }
+
   function renderPopup() {
+    initManageDialog();
     chrome.windows.getAll( {populate: true}, function( windowList ) {
       console.log( "in windows.getAll callback:", windowList );
       var managedWindows = subjectConfig; // for now
       var unmanagedWindows = windowList;  // for now
 
-      // renderManagedList( managedWindows );
+      renderManagedList( managedWindows );
       renderUnmanagedList( unmanagedWindows );
     } );
   }
