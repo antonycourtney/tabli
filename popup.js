71f4b69debbc63f30991bd74de4eba5e8cbe1424
Antony Courtney <antony.courtney@gmail.com>
2014-03-06 14:43:57 -0800

    set up background page and sync window list

---
+++ popup.js
@@ -0,0 +1,197 @@
+(function ($) {
+  'use strict';
+  $.extend( true, window, {
+    tabMan: {
+      renderPopup: renderPopup
+    }
+  });
+
+  var bgw = chrome.extension.getBackgroundPage();
+
+  function insertAfter(referenceNode, newNode) {
+      referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+  }
+
+/*
+  function updateWindowList() {
+    var subjectWindowList = [];
+    var unmanagedWindowList = [];
+
+    var subjectWindows = {};
+    var subjectMatchers = [];
+
+    function init() {
+      var subject;
+      for ( subject in subjectConfig ) {
+        if ( !subjectConfig.hasOwnProperty( subject ) ) 
+          continue;
+        var sc = subjectConfig[ subject ];
+        var pat = sc.urlHostSuffix.replace(".", "\\.") + "$";
+        var re = new RegExp( pat );
+        var sw = { subject: subject, config: sc };
+        subjectWindowList.push( sw );
+        subjectMatchers.push( { urlRE: re, subjectWindow: sw } );
+      }
+    }
+
+
+
+    function buildWindowLists( windowList ) {
+      console.log( "tabMan.buildWindowLists ", windowList );
+      for ( var i = 0; i < windowList.length; i++ ) {
+        var win = windowList[i];
+        var tabs = win.tabs;
+        for ( var j = 0; j < tabs.length; j++ ) {
+          var tab = tabs[j];
+          var sw = findSubjectByURL( tab.url );
+
+        }
+      }
+    }
+    // chrome.windows.getAll( {populate: true}, buildWindowLists );
+*/
+
+
+  function initManageDialog() {
+    var subject = $("#subject"),
+        allFields = $( [] ).add( subject );
+    $("#manage-dialog").dialog({
+          autoOpen: false,
+          height: 200,
+          width: 280,
+          modal: true,
+          buttons: {
+            "Create": function() {
+              var bValid = true;
+              allFields.removeClass( "ui-state-error" );
+
+              if ( bValid ) {
+                var tabWindow = $( this ).data( "tabWindow" );
+                console.log( "manage window:", tabWindow );
+                var subjField = $( "#subject" );
+                var windowTitle = subjField.val();
+                bgw.tabMan.manageWindow( tabWindow, { title: windowTitle } );
+                $( this ).dialog( "close" );
+              }
+            },
+            Cancel: function() {
+              $( this ).dialog( "close" );
+            }
+          },
+          close: function() {
+            allFields.val( "" ).removeClass( "ui-state-error" );
+          }
+        }); 
+  }
+
+  function makeElem( tag, options ) {
+    var item = document.createElement( tag );
+    if( options ) {
+      var txt = options.text;
+      if( txt )
+        item.appendChild( document.createTextNode(txt) );
+      var classes = options.classes;
+      if( classes ) {
+        for ( var i = 0; i < classes.length; i++ ) {
+          item.classList.add( classes[ i ] );
+        }
+      }
+      var parent = options.parent;
+      if( parent ) {
+        parent.appendChild( item );
+      }
+      var attrs = options.attributes;
+      if( attrs ) {
+        for( var attr in attrs ) {
+          if( !attrs.hasOwnProperty( attr ) )
+            continue;
+          item.setAttribute( attr, attrs[attr] );
+        }
+      }
+    }
+
+    return item;
+  };
+  
+  function renderTabWindow( tabWindow ) {
+    var managed = tabWindow.managed;
+    var windowTitle = tabWindow.getTitle();
+    var tabs = tabWindow.chromeWindow.tabs;
+
+    var headerId = managed ? 'managedWindows' : 'unmanagedWindows';
+    var windowItem = makeElem( 'div', { classes: [ "windowInfo" ] } );
+
+    var windowHeader = makeElem( 'div', 
+      { classes: [ "nowrap", "singlerow", "oneRowContainer", "windowHeader" ] } );
+
+    var windowTitleItem = makeElem( 'span', 
+      { text: windowTitle, 
+        classes: [ "windowList", "nowrap", "singlerow", "windowTitle" ],
+        parent: windowHeader 
+      });
+
+    if( !managed ) {
+      var windowManageButton = makeElem( 'button', 
+        { classes: [ "manage", "header-button" ],
+          attributes: { 'title': "Manage as Subject Window" },
+          parent: windowHeader 
+        });
+      windowManageButton.onclick = function() {
+        console.log( "Manage window '" + windowTitle + "'" );
+        var dlg = $("#manage-dialog" );
+        var subjField = $( "#subject" );
+        subjField.val( windowTitle );
+        window.setTimeout( function() {
+          subjField[0].setSelectionRange( 0, windowTitle.length );
+        }, 0 );
+        dlg.data( "tabWindow", tabWindow );
+        dlg.dialog( "open" );        
+      }
+    }
+    var tabListItem = makeElem('div', { classes: [ "tablist" ] } );
+    for( var i = 0; i < tabs.length; i++ ) {
+      var tab = tabs[ i ];
+
+      var tabItem = makeElem( 'div', 
+        { classes: [ "singlerow", "nowrap", "oneRowContainer", "tabinfo" ] } );
+      if( tab.favIconUrl ) {
+        var tabFavIcon = makeElem('img', { classes: [ "favicon" ], parent: tabItem } );
+        tabFavIcon.setAttribute( 'src', tab.favIconUrl );
+      }
+
+      var titleItem = makeElem( 'span', 
+        { text: tab.title,
+          classes: [ "windowList", "nowrap", "singlerow" ],
+          parent: tabItem
+        });
+
+      tabListItem.appendChild( tabItem );
+    }
+    windowItem.appendChild( windowHeader ); 
+    windowItem.appendChild( tabListItem );
+
+    var winHeader= document.getElementById( headerId );
+    insertAfter( winHeader, windowItem );
+  }
+
+  function renderPopup() {
+    initManageDialog();
+    console.log( "background page:", bgw );
+    chrome.windows.getAll( {populate: true}, function( windowList ) {
+      console.log( "in windows.getAll callback:", windowList );
+      var tabWindows = bgw.tabMan.syncWindowList( windowList );
+      console.log( "tabWindows:", tabWindows );
+      for ( var i = 0; i < tabWindows.length; i++ ) {
+        renderTabWindow( tabWindows[ i ] );
+      }
+    } );
+  }
+})(jQuery);
+
+/*
+document.addEventListener('DOMContentLoaded', function () {
+  tabMan.renderPopup();
+});
+
+*/
+$(document).bind('ready', tabMan.renderPopup );
--- tabman.js
+++ tabman.js
@@ -3,35 +3,72 @@
   $.extend( true, window, {
     tabMan: {
       parseURL: parseURL,
-      renderPopup: renderPopup
+      syncWindowList: syncWindowList,
+      manageWindow: manageWindow,
     }
   });
 
-  // TODO: construct and store dynamically!
-  var subjectConfig = {
-    'Mail': {
-      urlHostSuffix: 'mail.google.com',
-      tabs: [
-        { title: "GMail Inbox", url: "https://mail.google.com/mail/u/0/#inbox" }
-      ],
-    },
-    'Facebook': {
-      urlHostSuffix: 'facebook.com',
-      tabs: [
-        { title: "Facebook", url: "https://www.facebook.com/" }
-      ] 
-    },
-    'Python': {
-      urlHostSuffix: 'docs.python.org',
-      tabs: [
-        { title: "Python Docs", url: "http://docs.python.org/2/" },
-        { title: "Tutorial", url: "http://docs.python.org/2/tutorial/index.html" },
-        { title: "Library Reference", url: "http://docs.python.org/2/library/index.html" },
-        { title: "Language Reference", url: "http://docs.python.org/2/reference/index.html" }
-      ]  
-    }
-  };
+  var windowIdMap = {};
+  var tabWindows = [];
 
+
+  function getTabWindowTitle() {
+    if( this.managed ) {
+      return this._managedTitle;
+    } else {
+      var tabs = this.chromeWindow.tabs;
+      // linear search to find active tab to use as window title
+      for ( var j = 0; j < tabs.length; j++ ) {
+        var tab = tabs[j];
+        if ( tab.active ) {
+          return tab.title;
+        }
+      }
+    }
+    return "";  // shouldn't happen
+  }
+
+ /*
+  * begin managing the specified tab window
+  */
+  function manageWindow( tabWindow, opts ) {
+    tabWindow.managed = true;
+    tabWindow._managedTitle = opts.title;
+  }
+
+  /*  
+   * initialize a tab window from a chrome Window
+   */
+  function makeTabWindow( chromeWindow ) {
+    var ret = { managed: false, chromeWindow: chromeWindow, _managedTitle: "" };
+    ret.getTitle = getTabWindowTitle;
+    return ret;
+  }
+
+  /**
+   * synchronize windows from chrome.windows.getAll with internal map of
+   * managed and unmanaged tab windows
+   * returns:
+   *   - array of all tab Windows
+   */
+  function syncWindowList( chromeWindowList ) {
+    for ( var i = 0; i < chromeWindowList.length; i++ ) {
+      var chromeWindow = chromeWindowList[ i ];
+      var tabWindow = windowIdMap[ chromeWindow.id ];
+      if( !tabWindow ) {
+        console.log( "syncWindowList: new window id: ", chromeWindow.id );
+        tabWindow = makeTabWindow( chromeWindow );
+        windowIdMap[ chromeWindow.id ] = tabWindow;
+        tabWindows.push( tabWindow );
+      } else {
+        console.log( "syncWindowList: cache hit for id: ", chromeWindow.id );
+        // Set chromeWindow to current snapshot of tab contents:
+        tabWindow.chromeWindow = chromeWindow;
+      }
+    }
+
+    return tabWindows;
+  }   
 
   // This function creates a new anchor element and uses location
   // properties (inherent) to get the desired URL data. Some String
@@ -65,223 +102,6 @@
       };
   }
 
-  function insertAfter(referenceNode, newNode) {
-      referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-  }
 
 
-/*
-  function updateWindowList() {
-    var subjectWindowList = [];
-    var unmanagedWindowList = [];
-
-    var subjectWindows = {};
-    var subjectMatchers = [];
-
-    function init() {
-      var subject;
-      for ( subject in subjectConfig ) {
-        if ( !subjectConfig.hasOwnProperty( subject ) ) 
-          continue;
-        var sc = subjectConfig[ subject ];
-        var pat = sc.urlHostSuffix.replace(".", "\\.") + "$";
-        var re = new RegExp( pat );
-        var sw = { subject: subject, config: sc };
-        subjectWindowList.push( sw );
-        subjectMatchers.push( { urlRE: re, subjectWindow: sw } );
-      }
-    }
-
-
-
-    function buildWindowLists( windowList ) {
-      console.log( "tabMan.buildWindowLists ", windowList );
-      for ( var i = 0; i < windowList.length; i++ ) {
-        var win = windowList[i];
-        var tabs = win.tabs;
-        for ( var j = 0; j < tabs.length; j++ ) {
-          var tab = tabs[j];
-          var sw = findSubjectByURL( tab.url );
-
-        }
-      }
-    }
-    // chrome.windows.getAll( {populate: true}, buildWindowLists );
-*/
-  function makeElem( tag, options ) {
-    var item = document.createElement( tag );
-    if( options ) {
-      var txt = options.text;
-      if( txt )
-        item.appendChild( document.createTextNode(txt) );
-      var classes = options.classes;
-      if( classes ) {
-        for ( var i = 0; i < classes.length; i++ ) {
-          item.classList.add( classes[ i ] );
-        }
-      }
-      var parent = options.parent;
-      if( parent ) {
-        parent.appendChild( item );
-      }
-      var attrs = options.attributes;
-      if( attrs ) {
-        for( var attr in attrs ) {
-          if( !attrs.hasOwnProperty( attr ) )
-            continue;
-          item.setAttribute( attr, attrs[attr] );
-        }
-      }
-    }
-
-    return item;
-  };
-  
-  function renderWindow( managed, windowTitle, tabs ) {
-    var headerId = managed ? 'managedWindows' : 'unmanagedWindows';
-    var windowItem = makeElem( 'div', { classes: [ "windowInfo" ] } );
-
-    var windowHeader = makeElem( 'div', 
-      { classes: [ "nowrap", "singlerow", "oneRowContainer", "windowHeader" ] } );
-
-    var windowTitleItem = makeElem( 'span', 
-      { text: windowTitle, 
-        classes: [ "windowList", "nowrap", "singlerow", "windowTitle" ],
-        parent: windowHeader 
-      });
-
-    if( !managed ) {
-      var windowManageButton = makeElem( 'button', 
-        { classes: [ "manage", "header-button" ],
-          attributes: { 'title': "Manage as Subject Window" },
-          parent: windowHeader 
-        });
-      windowManageButton.onclick = function() {
-        console.log( "Manage window '" + windowTitle + "'" );
-        var subjField = $( "#subject" );
-        subjField.val( windowTitle );
-        window.setTimeout( function() {
-          subjField[0].setSelectionRange( 0, windowTitle.length );
-        }, 0 );
-        $( "#manage-dialog" ).dialog( "open" );        
-      }
-    }
-    var tabListItem = makeElem('div', { classes: [ "tablist" ] } );
-    for( var i = 0; i < tabs.length; i++ ) {
-      var tab = tabs[ i ];
-
-      var tabItem = makeElem( 'div', 
-        { classes: [ "singlerow", "nowrap", "oneRowContainer", "tabinfo" ] } );
-      if( tab.favIconUrl ) {
-        var tabFavIcon = makeElem('img', { classes: [ "favicon" ], parent: tabItem } );
-        tabFavIcon.setAttribute( 'src', tab.favIconUrl );
-      }
-
-      var titleItem = makeElem( 'span', 
-        { text: tab.title,
-          classes: [ "windowList", "nowrap", "singlerow" ],
-          parent: tabItem
-        });
-
-      tabListItem.appendChild( tabItem );
-    }
-    windowItem.appendChild( windowHeader ); 
-    windowItem.appendChild( tabListItem );
-
-    var winHeader= document.getElementById( headerId );
-    insertAfter( winHeader, windowItem );
-  }
-
-  function renderManagedList( managedWindows ) {
-    var subject;
-    for ( subject in subjectConfig ) {
-      if ( !subjectConfig.hasOwnProperty( subject ) ) 
-        continue;
-      var sc = subjectConfig[ subject ];
-
-      renderWindow( true, subject, sc.tabs );
-    }
-  }
-
-  function renderUnmanagedList( windowList ) {
-    for ( var i = 0; i < windowList.length; i++ ) {
-      var win = windowList[i];
-      var tabs = win.tabs;
-      var windowTitle = "";
-
-      // linear search to find active tab to use as window title
-      for ( var j = 0; j < tabs.length; j++ ) {
-        var tab = tabs[j];
-        if ( tab.active ) {
-          windowTitle = tab.title;
-        }
-      }
-      
-      renderWindow( false, windowTitle, tabs );
-    }          
-  }
-
-  function initManageDialog() {
-    var subject = $("#subject"),
-        allFields = $( [] ).add( subject );
-    $("#manage-dialog").dialog({
-          autoOpen: false,
-          height: 200,
-          width: 280,
-          modal: true,
-          buttons: {
-            "Create": function() {
-              var bValid = true;
-              // allFields.removeClass( "ui-state-error" );
-
-/*
-              bValid = bValid && checkLength( name, "username", 3, 16 );
-              bValid = bValid && checkLength( email, "email", 6, 80 );
-              bValid = bValid && checkLength( password, "password", 5, 16 );
-
-              bValid = bValid && checkRegexp( name, /^[a-z]([0-9a-z_])+$/i, "Username may consist of a-z, 0-9, underscores, begin with a letter." );
-              // From jquery.validate.js (by joern), contributed by Scott Gonzalez: http://projects.scottsplayground.com/email_address_validation/
-              bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com" );
-              bValid = bValid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
-*/
-              if ( bValid ) {
-/*
-                $( "#users tbody" ).append( "<tr>" +
-                  "<td>" + name.val() + "</td>" +
-                  "<td>" + email.val() + "</td>" +
-                  "<td>" + password.val() + "</td>" +
-                "</tr>" );
-*/
-                $( this ).dialog( "close" );
-              }
-            },
-            Cancel: function() {
-              $( this ).dialog( "close" );
-            }
-          },
-          close: function() {
-            allFields.val( "" ).removeClass( "ui-state-error" );
-          }
-        }); 
-  }
-
-  function renderPopup() {
-    initManageDialog();
-    chrome.windows.getAll( {populate: true}, function( windowList ) {
-      console.log( "in windows.getAll callback:", windowList );
-      var managedWindows = subjectConfig; // for now
-      var unmanagedWindows = windowList;  // for now
-
-      renderManagedList( managedWindows );
-      renderUnmanagedList( unmanagedWindows );
-    } );
-  }
 })(jQuery);
-
-/*
-document.addEventListener('DOMContentLoaded', function () {
-  tabMan.renderPopup();
-});
-
-*/
-$(document).bind('ready', tabMan.renderPopup );
