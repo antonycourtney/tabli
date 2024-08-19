310a820a26d8bf3348e5019242cb89d4c4435a45
Antony Courtney <antony.courtney@gmail.com>
2024-08-18 21:20:07 -0700

    Force fix for Rel Notes styling

--- src/ts/components/MessageCard.tsx
+++ src/ts/components/MessageCard.tsx
@@ -3,6 +3,7 @@ import * as styles from './cssStyles';
 import { cx, css } from '@emotion/css';
 import FlatButton from './FlatButton';
 import { Theme, ThemeContext } from './themeContext';
+import { useEffect } from 'react';
 
 const messageCardBaseStyle = css({
     padding: 0,
@@ -11,7 +12,7 @@ const messageCardStyle = (theme: Theme) =>
     cx(
         styles.tabWindow(theme),
         styles.tabWindowFocused(theme),
-        messageCardBaseStyle
+        messageCardBaseStyle,
     );
 
 const cardActionsStyle = css({
@@ -38,6 +39,14 @@ const MessageCard: React.FunctionComponent<MessageCardProps> = ({
 }: MessageCardProps) => {
     const rawMarkup = { __html: content };
     const theme = React.useContext(ThemeContext);
+
+    useEffect(() => {
+        // Force the CSS variable for crufty Blueprint card style:
+        document.documentElement.style.setProperty(
+            '--bs-body-color',
+            theme.foreground,
+        );
+    }, []);
 
     return (
         <div className={messageCardStyle(theme)}>
